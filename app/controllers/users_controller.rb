bc39e63b03601df4e93d18280d4eabd958ef0b37
George Devedlaka <gdevpri123@gmail.com>
2016-11-14 16:35:07 +0000

    Created user_controller and index.html.erb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def update
+
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161114141928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "space_required"
+    t.integer  "user_id"
+    t.integer  "storage_space_id"
+    t.boolean  "accepted"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["storage_space_id"], name: "index_bookings_on_storage_space_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "storage_spaces", force: :cascade do |t|
+    t.string   "location"
+    t.integer  "space"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_storage_spaces_on_user_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookings", "storage_spaces"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "storage_spaces", "users"
 end
