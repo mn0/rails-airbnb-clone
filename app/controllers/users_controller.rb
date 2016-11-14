97e45a41de6d864da28314523e11d2dc6435d3a4
George Devedlaka <gdevpri123@gmail.com>
2016-11-14 17:21:18 +0000

    Added index, create, update and strong params to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-
+    @users = User.all
   end
 
   def create
-
+    @user = User.new(user_params)
+    @user.save
   end
 
   def edit
@@ -17,7 +18,16 @@ class UsersController < ApplicationController
   end
 
   def update
-
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
+  private
+
+  def user_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:user).permit(:username)
+  end
 end
