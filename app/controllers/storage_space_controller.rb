7af4bdaa7379c462d8688aef3fac693a25030df5
Matthew Noble <matt_noble@hotmail.com>
2016-11-15 15:20:51 +0000

    storage space controller created

--- app/controllers/storage_space_controller.rb
+++ app/controllers/storage_space_controller.rb
@@ -1,10 +1,20 @@
 class StorageSpaceController < ApplicationController
   def index
-  end
-
-  def show
+    @storage_spaces = StorageSpace.all
   end
 
   def create
+    @storage_space = StorageSpace.new(storage_space_params)
+    @storage_space.save
+  def update
+    @storage_space = StorageSpace.find(params[:id])
+    @storage_space.update(storage_space_params)
+    redirect_to storage_space_path(@storage_space)
+  end
+
+  private
+
+  def storage_space_params
+    params.require(:storage_space).permit(:username)
   end
 end
