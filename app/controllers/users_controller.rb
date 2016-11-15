9806f5ed7ccb9bfcc32e9829d6774497795d866e
George Devedlaka <gdevpri123@gmail.com>
2016-11-15 11:34:24 +0000

    Indentation ect fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,6 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.all
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.save
-  end
-
   def edit
-
-  end
-
-  def new
 
   end
 
