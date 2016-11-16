85fb7e49e6cb2333b9cffc6e9184aca92552b462
Matthew Noble <matt_noble@hotmail.com>
2016-11-16 12:41:31 +0000

    fake seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(username: "alex")
+User.create!(username: "alex", email: "alex@alex.com", password: "password1")
 StorageSpace.create!(location: '22 fayke street, london, eb3 4rx', space: 34)
 StorageSpace.create!(location: '11 fayke street, london, eb3 4rx', space: 80)
 StorageSpace.create!(location: '33 fayke street, london, eb3 4rx', space: 1000)
