423557b72293d21a9439e07fc289f30af8d7c31b
Matthew Noble <matt_noble@hotmail.com>
2016-11-18 16:02:30 +0000

    added new db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 alex = User.create!(username: "alex", email: "alex@alex.com", password: "password1")
+jojo = User.create!(username: "jojo", email: "jojo@jo.com", password: "jojojo")
+george = User.create!(username: "george", email: "george@george.com", password: "george123")
+matt = User.create!(username: "matt", email: "matt@matt.com", password: "matt123")
+
+
+
 
 StorageSpace.create!(user: alex, location: '22 elder st, london', space: 34)
-StorageSpace.create!(user: alex, location: '11 downing st, london', space: 80)
-StorageSpace.create!(user: alex, location: '1 knightsbridge, london', space: 1000)
+StorageSpace.create!(user: jojo, location: '11 downing st, london', space: 80)
+StorageSpace.create!(user: george, location: '1 knightsbridge, london', space: 1000)
 
-puts "Seeded 3 new spaces."
+Booking.create!(user_id: matt, storage_space_id: alex, start_date: "2016-03-12", end_date: "2016-03-19", space_required: 20)
+Booking.create!(user_id: jojo, storage_space_id: george, start_date: "2016-03-12", end_date: "2016-03-19", space_required: 30)
+Booking.create!(user_id: alex, storage_space_id: jojo, start_date: "2016-03-12", end_date: "2016-03-19", space_required: 30)
+
+
+puts "Seeded 3 new spaces, 4 new users, 3 bookings."
